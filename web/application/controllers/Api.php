c977f5b7d42486a68dfb6d8fdacca0c78f0e2bc7
Ricardo Garcia <Rick@Ricardos-Mac-mini.local>
2017-10-03 15:19:25 -0500

    [Web] Api controller

---
+++ web/application/controllers/Api.php
@@ -0,0 +1,19 @@
+<?php
+
+class Api extends CI_Controller{
+
+  function __construct(){
+    parent::__construct();
+  }
+
+  function checkUserByEmail(){
+
+  }
+
+  function registerUser(){
+
+  }
+
+
+
+}
