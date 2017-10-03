2e67a8b31292899bca802c5a97f2a1cb5d94dacc
Ricardo Garcia <Rick@Ricardos-Mac-mini.local>
2017-10-03 14:07:43 -0500

    [Web] Unique_edit function added at Form_validations library

--- web/system/libraries/Form_validation.php
+++ web/system/libraries/Form_validation.php
@@ -1123,6 +1123,16 @@ class CI_Form_validation {
 			: FALSE;
 	}
 
+	public function edit_unique($str, $field)
+	{
+			sscanf($field, '%[^.].%[^.].%[^.]', $table, $field, $id);
+			return isset($this->CI->db)
+					? ($this->CI->db->limit(1)->get_where($table, array($field => $str, 'id !=' => $id))->num_rows() === 0)
+					: FALSE;
+	}
+
+
+
 	// --------------------------------------------------------------------
 
 	/**
