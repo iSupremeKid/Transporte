19a3d1c3eb49b820eced78722697a83a6481daa1
Ricardo Garcia <Rick@Ricardos-Mac-mini.local>
2017-10-06 15:49:43 -0500

    [Web] Function registerUser implemented for API

--- web/application/controllers/Api.php
+++ web/application/controllers/Api.php
@@ -31,5 +31,37 @@ class Api extends CI_Controller{
   }
 
 
+  function registerUser(){
+    $this->load->model("Persona_model");
+
+    $params = array(
+      'telefono' => $this->input->post('telefono'),
+      'persona_perfil_id' => 1,
+      'nombres' => $this->input->post('nombres'),
+      'apellido_paterno' => $this->input->post('apepat'),
+      'apellido_materno' => $this->input->post('apemat'),
+      'identificacion' => $this->input->post('dni'),
+      'saldo_disponible' => 0,
+      'estado' => 1,
+    );
+
+    $persona_id = $this->Persona_model->add_persona($params);
+
+    if ($persona_id) {
+      return $this->output
+              ->set_content_type('application/json')
+              ->set_output(json_encode(array(
+                      'success' => true
+              )));
+    }else{
+      return $this->output
+              ->set_content_type('application/json')
+              ->set_output(json_encode(array(
+                      'success' => false
+              )));
+    }
+    
+  }
+
 
 }
