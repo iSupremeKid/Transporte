902f65f3f393d4cc7c0348af1f5c1dc773e7a029
José Carlos Quichiz Santome <josec.quichiz@gmail.com>
2017-10-06 17:37:31 -0500

    [App pasajero] Login stack

--- pasajero-app/www/js/index.js
+++ pasajero-app/www/js/index.js
@@ -1,46 +1,133 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+$(document).on('deviceready',function(){
+    var global_data = {}
+    if(!!window.localStorage.getItem("id")){
+        $.mobile.navigate( "#main" ,{});
     }
-};
 
-app.initialize();
+    // User phone
+    $("#loginPhoneForm")
+    .submit(function(e){
+        e.preventDefault();
+        $.mobile.loading('show', {
+          theme: "b"
+        });
+        var number = $(this)
+        .find("#passenger_phone")
+        .val();
+        axios({
+          method: 'post',
+          url: 'https://api.authy.com/protected/json/phones/verification/start',
+          data: {
+            via: 'sms',
+            phone_number: number,
+            country_code: 51,
+            locale: 'es'
+          },
+          headers: {
+            "X-Authy-API-Key": "PnKtYn547C8GysxGUiX8OZIaRe9DLzTM"
+          }
+        })
+        .then(function(response){
+            response = response.data;
+            if(response.success === true){
+                $.mobile.loading('hide');
+                global_data = {number: number}
+                $.mobile.navigate( "#confirmPhone" ,{});
+            }else{
+                alert("Ocurrio un error!")
+            }
+        },function(err){
+            $.mobile.loading('hide');
+            alert("Error!")
+        });
+    })
+
+    // Verify number
+    $("#validatePhoneForm")
+    .submit(function(e){
+        e.preventDefault();
+        $.mobile.loading('show', {
+          theme: "b"
+        });
+        var validation = $(this)
+        .find("#validation_code")
+        .val();
+        axios({
+
+          method: 'get',
+          url: 'https://api.authy.com/protected/json/phones/verification/check?phone_number=' + global_data.number + '&country_code=51&verification_code=' + validation,
+          data: {
+          },
+          headers: {
+            "X-Authy-API-Key": "PnKtYn547C8GysxGUiX8OZIaRe9DLzTM"
+          }
+        })
+        .then(function(response){
+            response = response.data;
+            if(response.success === true){
+                // Numero correcto
+                // TODO: hacer request a API preguntando si existe el usuario y guardar en resultado en la variable user
+                
+                axios({
+                    method: "get",
+                    url: "http://www.rick-garcia.com/Transporte/api/checkUserExist/" + global_data.number,
+                    data: {}
+                })
+                .then(function(response){
+                    var user = response.data;
+                    $.mobile.loading('hide');
+                    console.log(user)
+                    if(user.success){
+                        window.localStorage.setItem("nombre", user.data.nombres + " " + user.data.apellido_paterno);
+                        window.localStorage.setItem("dni", user.data.identificacion);
+                        window.localStorage.setItem("id", user.data.id);
+                        $.mobile.navigate( "#main" ,{});
+                    }else{
+                        $.mobile.navigate( "#registerUser" ,{});
+                    }
+
+                })
+                
+            }else{
+                alert("Codigo incorrecto!")
+            }
+        },function(){
+            $.mobile.loading('hide');
+            alert("Codigo incorrecto!")
+        });
+    })
+
+
+    $("#registerUserForm")
+    .submit(function(e){
+        e.preventDefault();
+        var nuevos_datos = $(this).serializeArray();
+        nuevos_datos.push({name:"telefono",value: global_data.number});
+        console.log("enviando",nuevos_datos)
+        axios.post('http://www.rick-garcia.com/Transporte/api/registerUser',$.param(nuevos_datos))
+        .then(function(response){
+            user = response.data;
+            window.localStorage.setItem("nombre", user.data.nombres + " " + user.data.apellido_paterno);
+            window.localStorage.setItem("dni", user.data.identificacion);
+            window.localStorage.setItem("id", user.data.id);
+            $.mobile.navigate( "#main" ,{});
+        })
+    })
+
+
+    $( document ).on( "pagecreate", "#main", function() {
+        $( document ).on( "swipeleft swiperight", "#main", function( e ) {
+            // We check if there is no open panel on the page because otherwise
+            // a swipe to close the left panel would also open the right panel (and v.v.).
+            // We do this by checking the data that the framework stores on the page element (panel: open).
+            if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+                if ( e.type === "swipeleft" ) {
+                    $( "#sidebar" ).panel( "open" );
+                }
+                // else if ( e.type === "swiperight" ) {
+                //     $( "#left-panel" ).panel( "open" );
+                // }
+            }
+        });
+    });
+})
