91d60efa64e61b607a8f1017ff791c3f8d78b72d
Ricardo Garcia <Rick@Ricardos-Mac-mini.local>
2017-10-06 15:16:17 -0500

    [Web] Function checkUserExists implemented for API

--- web/application/controllers/Api.php
+++ web/application/controllers/Api.php
@@ -10,7 +10,23 @@ class Api extends CI_Controller{
 
   }
 
-  function registerUser(){
+  function checkUserExist($number){
+    $this->load->model("Persona_model");
+    $persona = $this->Persona_model->get_persona_by_number($number);
+    if ($persona) {
+      return $this->output
+              ->set_content_type('application/json')
+              ->set_output(json_encode(array(
+                      'success' => true,
+                      'persona' => $persona
+              )));
+    }else{
+      return $this->output
+              ->set_content_type('application/json')
+              ->set_output(json_encode(array(
+                      'success' => false
+              )));
+    }
 
   }
 
